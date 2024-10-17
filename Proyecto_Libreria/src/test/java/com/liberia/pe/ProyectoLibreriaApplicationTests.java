d21caa8e1e1b38e224325716db1e9c0deb54bc15
Samuel Cedillo <alvinetjosam@gmail.com>
2024-10-17 01:49:45 -0500

    Creacion del proyecto

---
+++ Proyecto_Libreria/src/test/java/com/liberia/pe/ProyectoLibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liberia.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
